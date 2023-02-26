7d97aae4b637bb7b693f68ce8a80fa26e3dcb3e4
Hoshi Takumi <hoshi.takumi.jp@gmail.com>
2023-02-26 18:56:22 +0900

    feat IDENTIFIER と VARIABLE を実装完了

--- verilog_filelist_parser/yacc_filelist.py
+++ verilog_filelist_parser/yacc_filelist.py
@@ -2,23 +2,20 @@ import ply.yacc as yacc
 from lex_filelist import tokens
 import os
 import pprint
-
-precedence = (
-    ('right', 'PLUS', 'EQUAL'),
-    ('right', 'DOLLER'),
-)
+import re
 
 def p_command(p):
     """command : arguments"""
     p[0] = p[1]
 
 def p_arguments(p):
-    """arguments : argument arguments
-               | argument"""
-    if len(p) == 2:
+    """arguments : argument SPACES arguments
+                 | argument SPACES
+                 | argument"""
+    if len(p) <= 3:
         p[0] = [p[1]]
     else:
-        for v in p[2:]:
+        for v in p[3:]:
             p[0] = [p[1]] + v
 
 def p_argument_positional(p):
@@ -33,8 +30,7 @@ def p_argument_positional(p):
 
 def p_argument_optional(p):
     """argument : SHORT_I factor
-                | SHORT_Y factor
-                | PLUS_INCDIR plus_factor"""
+                | SHORT_Y factor"""
     if p[1] == '+incdir':
         p[0] = {
             'tag': 'kIncludeArgument',
@@ -58,66 +54,65 @@ def p_argument_optional(p):
             ]
         }
 
-def p_plus_factor_identifier(p):
-    """plus_factor : PLUS IDENTIFIER plus_factor
-                   | PLUS IDENTIFIER"""
-    if len(p) == 3:
-        p[0] = [
-            {
+# def p_plus_factor_identifier(p):
+#     """plus_factor : PLUS IDENTIFIER plus_factor
+#                    | PLUS IDENTIFIER"""
+#     if len(p) == 3:
+#         p[0] = [
+#             {
+#                 'tag': 'identifier',
+#                 'text': p[2],
+#             }
+#         ]
+#     else:
+#         p[0] = [
+#             {
+#                 'tag': 'identifier',
+#                 'text': p[2],
+#             },
+#             *p[3],
+#         ]
+
+def p_factor_identifier(p):
+    """factor : variable factor
+              | IDENTIFIER factor
+              | variable
+              | IDENTIFIER"""
+    print(p[1])
+    if len(p) == 2:
+        if 'tag' in p[1]:
+            p[0] = {
                 'tag': 'identifier',
-                'text': p[2],
+                'text': os.environ[p[1]['text']],
             }
-        ]
-    else:
-        p[0] = [
-            {
+        else:
+            p[0] = {
                 'tag': 'identifier',
-                'text': p[2],
-            },
-            *p[3],
-        ]
+                'text': p[1],
+            }
+    else:
+        if 'tag' in p[1]:
+            p[0] = {
+                'tag': 'identifier',
+                'text': os.environ[p[1]['text']] + p[2]['text'],
+            }
+        else:
+            p[0] = {
+                'tag': 'identifier',
+                'text': p[1] + p[2]['text'],
+            }
 
-def p_factor_variable(p):
-    """factor : DOLLER factor
-              | DOLLER LPAREN factor RPAREN
-              | DOLLER LCURLY factor RCURLY"""
-    if p[2] == '(' or p[2] == '{':
+def p_variable(p):
+    """variable : VARIABLE"""
+    if re.match(r'\$[\(\{].+[\)\}]', p[1]):
         p[0] = {
-            'tag': p[3]['tag'],
-            'text': os.environ[p[3]['text']],
+            'tag': 'variable',
+            'text': p[1][2:-1],
         }
     else:
         p[0] = {
-            'tag': p[2]['tag'],
-            'text': os.environ[p[2]['text']],
-        }
-
-def p_factor_identifier(p):
-    """factor : IDENTIFIER
-              | IDENTIFIER PLUS factor"""
-    if len(p) == 2:
-        p[0] = {
-            'tag': 'identifier',
-            'text': p[1],
-        }
-    else:
-        p[0] = {
-            'tag': 'identifier',
-            'text': p[1] + p[2] + p[3]['text'],
-        }
-
-def p_factor_identifier(p):
-    """factor : IDENTIFIER
-              | IDENTIFIER PLUS factor"""
-    if len(p) == 2:
-        p[0] = {
-            'tag': 'identifier',
-            'text': p[1],
-        }
-    else:
-        p[0] = {
-            'tag': 'identifier',
-            'text': p[1] + p[2] + p[3]['text'],
+            'tag': 'variable',
+            'text': p[1][1:],
         }
 
 def p_error(p):
@@ -139,8 +134,12 @@ def main():
 #     data = '''
 # +define+macro1+macro2 -f run.f $(TB)/top_tb.sv ${SRC}/main.sv --top main -Ddef
 # '''
+#     data = '''
+# $OS src/test-Itest.sv src/test+test.sv -y src/test+test-Itest +incdir+tb1+tb2 +incdir+tb1 +incdir+tb2 +incdir+src1 src/-ysrc -y src -Iinclude -Iinc
+# '''
+
     data = '''
-$OS src/test-Itest.sv src/test+test.sv -y src/test+test-Itest +incdir+tb1+tb2 +incdir+tb1 +incdir+tb2 +incdir+src1 src/-ysrc -y src -Iinclude -Iinc
+$(OS) $OS/main.sv src/$OS/main.sv src/${OS} src/tb/main.sv sub.sv
 '''
     result = yacc.parse(data)
     print("  [%s] -> " % data)
